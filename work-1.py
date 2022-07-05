4a8e0abf150a8f9eb2af3c913b336895c550904a
Frenzytime <ofrenzytime@gmail.com>
2022-07-05 14:21:17 +0700

    first commit

---
+++ work-1.py
@@ -0,0 +1,11 @@
+print("Hello M.6/14 ")
+print("Pasika Moonsri")
+print("My nickname is Aum")
+print("I am 17 years old")
+print("my birthday is 22/09/2004 ")
+print("I study at Chonradsadornumrung school")
+print("My blodtyp is A")
+print("I like to eat papaya salad")
+print("IG:aum_pasi.")
+print("FB:Pasika Moonsri")
+#ชื่อ พศิกา มุลศรี เลขที่ 39 ชั้น ม.6/14
---
+++ work-2.py
@@ -0,0 +1,21 @@
+Name="Pasika"
+print(Name)
+Surname="Moonsri"
+print(Surname)
+Nickname="Aum"
+print(Nickname)
+Age="17"
+print(Age)
+Birthday="22/09/2004"
+print(Birthday)
+Bloodtyp="A"
+print(Bloodtyp)
+Food="papayasalad"
+print(Food)
+School="Chonradsadornumrung school"
+print(School)
+IG="Aum_pasi"
+print(IG)
+FB="Pasika Moonsri"
+print(FB)
+#ชื่อ พศิกา มุลศรี เลขที่ 39 ชั้น ม.5/14
